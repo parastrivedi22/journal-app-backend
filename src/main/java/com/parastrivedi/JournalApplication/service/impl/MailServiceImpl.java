70da351dd5a66d9400ea29356314241ce7e530c1
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-05-30 22:45:01 +0530

    Add validation in the email service
    add some realworld validation in the email service to validate email and its contents

--- src/main/java/com/parastrivedi/JournalApplication/service/impl/MailServiceImpl.java
+++ src/main/java/com/parastrivedi/JournalApplication/service/impl/MailServiceImpl.java
@@ -5,24 +5,37 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+import com.parastrivedi.JournalApplication.exception.BadRequestException;
 import com.parastrivedi.JournalApplication.service.MailService;
 
 import lombok.extern.slf4j.Slf4j;
 
-
 @Component
 @Slf4j
-public class MailServiceImpl implements MailService
-{
+public class MailServiceImpl implements MailService {
 	
-	
+	public final String EMAIL_PATTERN = "(?i)[-a-zA-Z0-9+_][-a-zA-Z0-9+_.]*@[-a-zA-Z0-9][-a-zA-Z0-9.]*\\.[a-zA-Z]{2,30}";
+
 	@Autowired
 	private JavaMailSender mailSender;
 
 	@Override
-	public boolean sendEmail(String to, String subject, String text) {
+	public boolean sendEmail(String to, String subject, String text)  {
 		// TODO Auto-generated method stub
+
+		if (to == null || to.trim().equals("") 
+				|| subject == null || subject.trim().equals("")
+				|| text == null || text.trim().equals(""))
+		{
+			log.warn("{} : email content is invalid", MailServiceImpl.class);
+			throw new BadRequestException("email content is invalid");
+		}
 		
+		if(!to.matches(EMAIL_PATTERN)) {
+			log.warn("{} : email is invalid", MailServiceImpl.class);
+			throw new BadRequestException("email is invalid");
+		}
+
 		try {
 			SimpleMailMessage mail = new SimpleMailMessage();
 			mail.setTo(to);
@@ -30,16 +43,12 @@ public class MailServiceImpl implements MailService
 			mail.setText(text);
 			mailSender.send(mail);
 			return true;
-		}catch(Exception ex) {
-			log.error("{} : email has been not sent",MailServiceImpl.class );
+		} catch (Exception ex) {
+			log.warn("{} : email has been not sent", MailServiceImpl.class);
 			System.out.print(ex);
 		}
-		
-		
+
 		return false;
 	}
-
-	
-	
 
 }
