c5acab0d9416e66ac91dcb80dd8c2d301911c10f
parastrivedi22 <140546779+parastrivedi22@users.noreply.github.com>
2024-05-31 11:33:21 +0530

    Testing

--- src/test/java/com/parastrivedi/JournalApplication/controller/UserControllerTest.java
+++ src/test/java/com/parastrivedi/JournalApplication/controller/UserControllerTest.java
@@ -23,23 +23,23 @@ public class UserControllerTest {
 
 	@InjectMocks
 	private UserController userController;
-	
+
 	@Mock
 	private UserService userService;
-	
+
 	@Test
-	public void test() {
+	public void testGetAllUsers() {
 		ResponseEntity<List<User>> allUsers = userController.getAllUsers();
 		assertNotNull(allUsers);
 
 	}
-	
+
 	ObjectId id = new ObjectId("664f8195d29e4129831d658a");
-	
+
 	@Test
-	public void test2() {
-		User u  = new User();
-		
+	public void testegGUserById() {
+		User u = new User();
+
 		u.setId(id);
 		u.setPassword("hello");
 		u.setRole("USER");
@@ -49,8 +49,31 @@ public class UserControllerTest {
 		User user = userController.getUserById(id).getBody();
 		assertEquals(u, user);
 		assertNotNull(user);
-		
-	}
-	
-}
 
+	}
+
+	@Test
+	public void testUpdateUser() {
+		User u1 = new User();
+		User u2 = new User();
+
+		u1.setId(id);
+		u1.setPassword("hlo");
+		u1.setRole("adf");
+		u1.setUserName("adfda");
+		u1.setUserEmail("adfalk@gmail.com");
+
+		u2.setId(id);
+		u2.setPassword("hello");
+		u2.setRole("USER");
+		u2.setUserName("Paras");
+		u2.setUserEmail("paras@gmail.com");
+
+		when(userController.updateUser(any(), any()).getBody()).thenReturn(u2);
+		User body = userController.updateUser(id, u1).getBody();
+
+		assertNotNull(body);
+		assertEquals(u2, body);
+	}
+
+}
